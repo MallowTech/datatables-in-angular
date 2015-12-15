3c8217f04e38afbadff31b3d34d85b9618ed945d
Mallowdev <jayaprakash@mallow-tech.com>
2015-12-15 10:16:18 +0530

    Initial import

---
+++ app/datatable/datatable_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.datatable module', function() {
+
+  beforeEach(module('myApp.datatable'));
+
+  describe('datatable controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var view1Ctrl = $controller('View1Ctrl');
+      expect(view1Ctrl).toBeDefined();
+    }));
+
+  });
+});
